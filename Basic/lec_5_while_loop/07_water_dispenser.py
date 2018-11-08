9a848a08794d736c4f008397543118889d8cea02
Ramsess <dbzahariev@gmail.com>
2018-11-08 11:55:56 +0200

    Add new files

--- Basic/lec_5_while_loop/07_water_dispenser.py
+++ Basic/lec_5_while_loop/07_water_dispenser.py
@@ -8,3 +8,21 @@ if ml == 0:
     print(f"The dispenser has been tapped {taped} times.")
 else:
     print(f"{abs(ml):.0f}ml has been spilled.")
+
+# Without dict
+
+# ml = float(input())
+# taped = 0
+# while ml > 0:
+#     buttons_input = input()
+#     if buttons_input == "Easy":
+#         ml -= 50
+#     elif buttons_input == "Medium":
+#         ml -= 100
+#     elif buttons_input == "Hard":
+#         ml -= 200
+#     taped += 1
+# if ml == 0:
+#     print(f"The dispenser has been tapped {taped} times.")
+# else:
+#     print(f"{abs(ml):.0f}ml has been spilled.")
