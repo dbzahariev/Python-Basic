2b9d5ecfb82e744bbb99c6138c226ca93bb2e27d
Ramsess <dbzahariev@gmail.com>
2018-11-14 10:29:04 +0200

    Add new files

--- Basic/lec_6_for_loop/04_max_number.py
+++ Basic/lec_6_for_loop/04_max_number.py
@@ -1,6 +1,5 @@
-input_count = int(input())
 max_number = 0
-for i in range(0, input_count):
+for i in range(0, int(input())):
     new_number = int(input())
     if i == 0:
         max_number = new_number
